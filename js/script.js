75c896d753157f49741e624652e0d7aebf0c10a0
yuhonakayama <yuhoimaizumi@gmail.com>
2025-01-15 15:58:02 +0900

    フッターまで完了

--- js/script.js
+++ js/script.js
@@ -5,3 +5,26 @@ $(function () {
     $(".js-drawer").fadeToggle();
   });
 });
+
+// スワイパー
+const swiper = new Swiper(".mv-swiper", {
+  loop: true,
+  effect: "fade",
+  speed: 3000,
+  allowTouchMove: false,
+  autoplay: {
+    delay: 3000,
+  },
+});
+
+// アコーディオン
+$(function() {
+  $('.js-accordion .faq__question').on('click', function() {
+    $(this)
+      .toggleClass('is-open')
+      .next('.faq__answer')
+      .slideToggle(300)
+      .parent('.faq__item')
+      .toggleClass('is-open');
+  });
+});
